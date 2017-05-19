ec8c31d2ed97c7c4e059ca3b957e0420eecfe87d
Alexey Lizurchik <al.lizurchik@gmail.com>
2017-05-19 17:26:19 +0300

    Add tests

---
+++ __mocks__/request.js
@@ -0,0 +1,22 @@
+'use strict';
+
+module.exports = fakeRequest;
+
+function fakeRequest(options, callback) {
+  let response = {};
+
+  if (options.url.indexOf(fakeRequest.INVALID_URL) !== -1) {
+    // error during request
+    return callback('invalid url');
+  }
+
+  if (options.method === 'GET') {
+    response.body = options.url;
+  } else if (options.method === 'POST') {
+    response.body = options.body;
+  }
+
+  callback(null, response)
+}
+
+fakeRequest.INVALID_URL = '[INVALID_URL]';
--- test.js
+++ test.js
@@ -1,5 +1,70 @@
-import fn from './index';
+import request from './__mocks__/request';
+import logger from './index';
 
-test('title', () => {
-  expect(fn('es-next')).toBe(undefined);
+const url = 'http://localhost/accept-log';
+const UNSUPPORTED_METHOD = 'PUT';
+
+describe('Sends message over the network', () => {
+  test('GET', () => {
+    const config = {
+      url
+    };
+
+    const response = logger.bind({config})(2, ['hello', 'world']);
+    return expect(response).resolves.toMatchObject({
+      body: `${url}?l=error&m=hello%20world&to=3`
+    });
+  });
+
+  test('POST', () => {
+    const config = {
+      method: 'POST',
+      url
+    };
+
+    const response = logger.bind({config})(2, ['hello', 'world']);
+    return expect(response).resolves.toMatchObject({
+      body: '{"l":"error","m":"hello world","to":3}'
+    });
+  });
+});
+
+describe('Errors handling', () => {
+  test('Url not set', () => {
+    const config = {
+      suppressRemoteErrors: false
+    };
+
+    expect(() => logger.bind({config})(2, ['hello', 'world'])).toThrow('Url must be set');
+  });
+
+  test('Invalid method', () => {
+    const config = {
+      method: UNSUPPORTED_METHOD,
+      suppressRemoteErrors: false,
+      url
+    };
+
+    expect(() => logger.bind({config})(2, ['hello', 'world'])).toThrow(`Method ${UNSUPPORTED_METHOD} is not supported`);
+  });
+
+  test('Invalid url during request', () => {
+    const config = {
+      suppressRemoteErrors: false,
+      url: request.INVALID_URL
+    };
+
+    const response = logger.bind({config})(2, ['hello', 'world']);
+    return expect(response).rejects.toEqual('invalid url');
+  });
+
+  test('Supress: Invalid url during request', () => {
+    const config = {
+      suppressRemoteErrors: true,
+      url: request.INVALID_URL
+    };
+
+    const response = logger.bind({config})(2, ['hello', 'world']);
+    return expect(response).resolves.toEqual(undefined);
+  });
 });
